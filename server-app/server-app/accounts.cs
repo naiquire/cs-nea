2b5992212efebbc07e33a84f45f504b07f56cc6b
naiquire <148003925+naiquire@users.noreply.github.com>
2025-06-12 19:22:59 +0100

    encrypted passwords

--- server-app/server-app/accounts.cs
+++ server-app/server-app/accounts.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using server_app.databases;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace server_app
 {
@@ -12,13 +14,14 @@ namespace server_app
         }
         public async void loginRequest(string userID, string password)
         {
-            int success = database.loginRequest(userID, password);
+            int success = database.loginRequest(userID, hashPassword(password));
             await Clients.Caller.SendAsync("loginSuccess", success);
         }
         public async void accountRequest(string userID, string password)
         {
-            bool success = database.accountRequest(userID, password);
+            bool success = database.accountRequest(userID, hashPassword(password));
             await Clients.Caller.SendAsync("accountSuccess", success ? 1 : -1);
         }
+        private static string hashPassword(string input) => Encoding.UTF8.GetString(SHA512.HashData(Encoding.UTF8.GetBytes(input)));
     }
 }
--- server-app/server-app/obj/Debug/net8.0/server-app.AssemblyInfo.cs
+++ server-app/server-app/obj/Debug/net8.0/server-app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d545899e8d363b553e1835821c221f44d8be327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed739b65d8c7359ee260113101f1418f2bb28d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("server-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
