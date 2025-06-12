58460e6e4fd6cca5b47c80c7c99ea7e73e0aa97a
naiquire <148003925+naiquire@users.noreply.github.com>
2025-06-12 20:31:50 +0100

    database.cs beta complete

--- server-app/server-app/databases/database.cs
+++ server-app/server-app/databases/database.cs
@@ -1,20 +1,21 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.Data.Sqlite;
 
 namespace server_app.databases
 {
     // handles all requests to the SQL database
     public struct userData
     {
-        public double accuracy;
-        public TimeSpan time;
+        public string userID;
+        public Dictionary<char, (double, TimeSpan)> statistics;
         public string aboutMe;
         public DateTime dateCreated;
         public int rank;
     }
     public static class @database
     {
-        private static readonly string dbPath = @"C:\Users\boyss\Documents\General\Relay\github\cs-nea-app\server-app\server-app\databases\maindb.sqlite, Version=3";
-        private static SqliteConnection connection = new(dbPath);
+        private static readonly string dbPath = @"Data Source=C:\Users\boyss\Documents\General\Relay\github\cs-nea-app\server-app\server-app\databases\maindb.sqlite";
+        private static readonly SqliteConnection connection = new(dbPath);
 
         public static int loginRequest(string userID, string password)
         {
@@ -43,14 +44,12 @@ namespace server_app.databases
         {
             if (!userExists(userID))
             {
-                string query = "INSERT INTO userData VALUES(@userID, @password, @accuracy, @time, @aboutMe, @dateCreated, @rank)";
+                string query = "INSERT INTO userData VALUES(@userID, @password, @aboutMe, @dateCreated, @rank)";
                 using (var command = new SqliteCommand(query, connection))
                 {
                     command.Parameters.AddWithValue("@userID", userID);
                     command.Parameters.AddWithValue("@password", password);
-                    command.Parameters.AddWithValue("@accuracy", null);
-                    command.Parameters.AddWithValue("@time", null);
-                    command.Parameters.AddWithValue("@aboutMe", null);
+                    command.Parameters.AddWithValue("@aboutMe", "");
                     command.Parameters.AddWithValue("@dateCreated", DateTime.UtcNow);
                     command.Parameters.AddWithValue("@rank", 300);
 
@@ -79,26 +78,42 @@ namespace server_app.databases
                 return false;
             }
         }
-        public static userData? loadUserData(string userID)
+        public static userData loadUserData(string userID)
         {
-            string query = "SELECT * FROM userData WHERE userData.userID = @userID";
+            userData userData = new();
+            Dictionary<char, (double, TimeSpan)> statistics = [];
+
+            string query = "SELECT aboutMe, dateCreated, rank, localisation FROM userData WHERE userData.userID = @userID";
             using (var command = new SqliteCommand(query, connection))
             {
                 command.Parameters.AddWithValue("@userID", userID);
                 var reader = command.ExecuteReader();
                 while (reader.Read())
                 {
-                    return new userData()
-                    {
-                        accuracy = reader.GetDouble(0),
-                        time = reader.GetTimeSpan(1),
-                        aboutMe = reader.GetString(2),
-                        dateCreated = reader.GetDateTime(3),
-                        rank = reader.GetInt32(4),
-                    };
+                    userData.aboutMe = reader.GetString(0);
+                    userData.dateCreated = reader.GetDateTime(2);
+                    userData.rank = reader.GetInt32(3);
                 }
             }
-            return null;
+
+            query = "SELECT letter, accuracy, time FROM statistics WHERE userID = @userID";
+            using (var command = new SqliteCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@userID", userID);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    char letter = reader.GetChar(0);
+                    double accuracy = reader.GetDouble(1);
+                    TimeSpan time = reader.GetTimeSpan(2);
+
+                    statistics[letter] = (accuracy, time);
+                }
+            }
+
+            userData.userID = userID;
+            userData.statistics = statistics;
+            return userData;
         }
     }
 }
