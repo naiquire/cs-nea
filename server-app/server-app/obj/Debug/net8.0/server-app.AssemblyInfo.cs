bfc44690bfc3360f02f9800cbcf5c2d4040a438d
naiquire <naiquire2500@outlook.com>
2026-01-28 11:58:59 +0000

    docs

--- server-app/server-app/obj/Debug/net8.0/server-app.AssemblyInfo.cs
+++ server-app/server-app/obj/Debug/net8.0/server-app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d4d2318208f05a36e3d66e6b05e68fbeb52a848")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8deb9550359ad7c86fd2fb1ba0e30b9ac0e051c")]
 [assembly: System.Reflection.AssemblyProductAttribute("server-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
